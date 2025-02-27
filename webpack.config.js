9121b8e021d27c490062f5619172e41fbfedd316
Mandlenkosi Neville <mandlenkosi.neville@gmail.com>
2025-02-27 18:05:17 +0200

    added gitignore

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/js/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devServer: {
+    static: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css",
+    }),
+    new HtmlWebpackPlugin({
+      title: "App Title",
+      template: "./src/index.html",
+    }),
+  ],
+};
